44e7708c6a9a7e8901190873ca8c7a6e6f5c29ad
邓祥柏 <1760399176@qq.com>
2024-05-06 22:58:06 +0800

    feat: ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { UserAuthForm } from '@/components/UserAuthForm'
+import { getCurrentUser } from '@/lib/session'
 
-export default function Home() {
+export default async function Home() {
+  const user = await getCurrentUser()
+  console.log(user)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>button</Button>
+      <>Current User:{user}</>
+      <UserAuthForm />
     </main>
   )
 }
